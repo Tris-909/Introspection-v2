274d8063ec229a999dffa0eec754951ffe3f96bd
Tri Tran <tranminhtri9090@gmail.com>
2024-04-30 21:25:03 +0700

    (3) Adding skeleton for timeline and calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { Flex, Box } from "@chakra-ui/react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import dayjs from "dayjs";
 
-function App() {
-  return <div className="App">123</div>;
-}
+const Home = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const dates = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
 
-export default App;
+  return (
+    <Flex h="100vh" background={"#181a19"} flexDir={"column"}>
+      <Flex justifyContent={"center"} alignItems={"center"} mt="20px">
+        <DatePicker
+          selected={startDate}
+          onChange={(date: Date) => setStartDate(date)}
+          dateFormat="MM/yyyy"
+          showMonthYearPicker
+          showFullMonthYearPicker
+        />
+      </Flex>
+      <Flex justifyContent={"center"} alignItems={"center"} h="98vh">
+        <Flex
+          h="3px"
+          w="100%"
+          borderTop="2px dashed white"
+          m="20px 50px"
+          flexDir="row"
+          justifyContent={"space-evenly"}
+          alignItems={"center"}
+        >
+          {dates.map((date) => (
+            <Box
+              key={date}
+              width="12px"
+              height="12px"
+              borderRadius="50%"
+              mb="4px"
+              bg="white"
+              cursor="pointer"
+            ></Box>
+          ))}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Home;
