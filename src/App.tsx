10dd4934e67ea8f4ed5d671b172271d3ae0e868c
Tri Tran <tranminhtri9090@gmail.com>
2024-05-01 14:33:43 +0700

    (4) Styling the Calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex, Box, Text } from "@chakra-ui/react";
 import DatePicker from "react-datepicker";
+import { CalendarContainer } from "react-datepicker";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 import "react-datepicker/dist/react-datepicker.css";
+import "./Calendar.css";
 import dayjs from "dayjs";
 
 const Home = () => {
@@ -10,6 +13,24 @@ const Home = () => {
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
   ];
+
+  const CalendarWrapper = ({
+    className,
+    children,
+  }: {
+    className: string;
+    children: React.ReactElement;
+  }) => {
+    return (
+      <div style={{ padding: "24px", background: "#fff" }}>
+        <CalendarContainer className={className}>
+          <div style={{ position: "relative", background: "#fff" }}>
+            {children}
+          </div>
+        </CalendarContainer>
+      </div>
+    );
+  };
 
   return (
     <Flex h="100vh" background={"#181a19"} flexDir={"column"}>
@@ -20,6 +41,50 @@ const Home = () => {
           dateFormat="MM/yyyy"
           showMonthYearPicker
           showFullMonthYearPicker
+          calendarContainer={CalendarWrapper}
+          renderCustomHeader={({
+            date,
+            monthDate,
+            customHeaderCount,
+            decreaseYear,
+            increaseYear,
+          }) => (
+            <Flex
+              flexDirection={"row"}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              bg="white"
+              mb="16px"
+            >
+              <ChevronLeftIcon
+                w="18"
+                h="18"
+                bg="white"
+                borderRadius="50%"
+                border="1px solid #131313"
+                onClick={decreaseYear}
+              />
+              <Text
+                color="#131313"
+                fontWeight={700}
+                fontSize={"18px"}
+                fontFamily={"Inter"}
+              >
+                {monthDate.toLocaleString("en-US", {
+                  month: "long",
+                  year: "numeric",
+                })}
+              </Text>
+              <ChevronRightIcon
+                w="18"
+                h="18"
+                bg="white"
+                borderRadius="50%"
+                border="1px solid #131313"
+                onClick={increaseYear}
+              />
+            </Flex>
+          )}
         />
       </Flex>
       <Flex justifyContent={"center"} alignItems={"center"} h="98vh">
